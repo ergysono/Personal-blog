8629a8165ee1aec375b88f225390e4f0afdbca93
Ergys <gysigba@gmail.com>
2022-08-07 14:43:40 +0200

    set up workspace

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { PostCard, Categories, PostWidget } from "../components";
+
+const posts = [
+  {title: 'React Testing', excerpt:'Learn React now'},
+  {title: 'React Testing number 2', excerpt:'Learn React now its too late'}
+]
+const Home: NextPage = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>CMS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post,index) => <PostCard post= {post} key={post.title} />)}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'>
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
