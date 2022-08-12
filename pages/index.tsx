178d99177d8e4a5ddb4596a540d15cb490a43cf4
Ergys <gysigba@gmail.com>
2022-08-12 19:15:52 +0200

    implemented query and env file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Key } from 'react';
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  {title: 'React Testing', excerpt:'Learn React now'},
-  {title: 'React Testing number 2', excerpt:'Learn React now its too late'}
-]
-const Home: NextPage = () => {
+interface Posts {
+     posts:  any[];
+}
+const Home : React.FC<Posts> = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,7 +17,7 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post,index) => <PostCard post= {post} key={post.title} />)}
+          {posts.map((post) => <PostCard post= {post.node} key={post.title} />)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
             <div className='lg:sticky relative top-8'>
@@ -29,4 +30,11 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Home;
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
